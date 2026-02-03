0e627495634b5b41343981cb9066a52615de6b59
JamesAdeshina <adeshina.james@bowen.edu.ng>
2026-02-03 18:11:04 +0000

    Update Terraform variables in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,19 @@
 variable "aws_region" {
   description = "AWS region"
-  default     = "us-east-1"
+  default     = "eu-north-1"  # Changed to Stockholm
 }
 
 variable "project_name" {
   description = "Project name for resource naming"
   default     = "medi-track-360"
+}
+
+variable "s3_bucket_name" {
+  description = "S3 bucket name for data lake"
+  default     = "medi-track-360-data-lake"
+}
+
+variable "redshift_cluster_identifier" {
+  description = "Existing Redshift cluster identifier"
+  default     = "redshift-cluster-1"
 }
